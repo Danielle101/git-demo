ee74741429a4fc8fc812cec0449381606692d403
DanielleHall <halldan4@yahoo.com>
2016-03-31 13:46:13 -0400

    Adding a new js file.

---
+++ danielle.js
@@ -0,0 +1,6 @@
+import java.util.Scanner;//import the scanner class
+
+public class mainClass {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Grand Circus' Room Detail Generator!");
